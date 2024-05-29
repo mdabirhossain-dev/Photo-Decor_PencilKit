37b5f4dc5f4b4d5528a75a2613e3f977463efa42
Md Abir Hossain <hossain.mdabir@gmail.com>
2024-05-29 11:23:12 +0600

    Project created and deployment target decreased to iOS 15 and MacOS 12.

---
+++ Photo Decor/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Photo Decor
+//
+//  Created by Md Abir Hossain on 29/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Photo Decor/Photo_DecorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Photo_DecorApp.swift
+//  Photo Decor
+//
+//  Created by Md Abir Hossain on 29/5/24.
+//
+
+import SwiftUI
+
+@main
+struct Photo_DecorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
